613f0920df8459a8251e87a16dd3b796f5c22aea
sdgu <sdgu000@gmail.com>
2016-12-28 21:46:38 -0800

    added a function to send to console and channel

--- bot.js
+++ bot.js
@@ -5,7 +5,20 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 var fs = require("fs");
 bot.login(pass);
+var console_chan_id = "263901361127686159";
 var lineCounts = JSON.parse(fs.readFileSync('lines.json', 'utf8'));
+
+
+var bot_activation_token_start = "(";
+var bot_activation_token_stop = ")";
+
+
+function double_console(text)
+{
+    console.log(text);
+    bot.channels.get(console_chan_id).sendMessage(text);
+}
+
 
 bot.on('ready', () => {
   console.log('I am ready!');
@@ -14,13 +27,13 @@ bot.on('ready', () => {
 bot.on("message", msg => {
     if(msg.author.bot) return;
     let lineCount = lineCounts[msg.author.id];
-    console.log(lineCount);
+    double_console(lineCount);
     if (!lineCount) {
         lineCount = 0;
-        console.log(lineCount);
+        double_console(lineCount);
     }
     lineCount++;
-    console.log(lineCount)
+    double_console(lineCount)
     fs.writeFile('lines.json', JSON.stringify(lineCounts), console.error);
 });
 
@@ -28,7 +41,7 @@ bot.on("message", msg => {
     if (msg.content.startsWith("ping")) {
         msg.channel.sendMessage("pong!");
     }
-    bot.channels.get("263901361127686159").sendMessage("thing");
+    
 });
 
 bot.on('error', e => { console.error(e); });
