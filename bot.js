ae0ad7fc65ea03e4b9b14a211f826312cff57e3d
Minus <kaushik.katta@gmail.com>
2016-12-29 04:34:28 +0000

    line counter 0.1

--- bot.js
+++ bot.js
@@ -4,15 +4,22 @@
 //===============================
 var Discord = require("discord.js");
 var bot = new Discord.Client();
+var fs = require("fs");
+bot.login("MjYzODI1MTY1Mzg3ODI1MTUz.C0YAsA.u_eQETAWuHQfMu2ot957ZepDAx8");
+var lineCounts = JSON.parse(fs.readFileSync('lines.json', 'utf8'));
 
 bot.on('ready', () => {
   console.log('I am ready!');
 });
 
 bot.on("message", msg => {
-    if (msg.content.startsWith("ping")) {
-        msg.channel.sendMessage("pong!");
+    if(msg.author.bot) return;
+    if (lineCounts[msg.author.id]) {
+        lineCounts[msg.author.id] = 1;
     }
+    lineCounts[msg.author.id]++;
+    console.log(lineCounts[msg.author.id])
+    fs.writeFile('lines.json', JSON.stringify(lineCounts), console.error);
 });
 
 bot.on("message", msg => {
@@ -22,5 +29,3 @@ bot.on("message", msg => {
 });
 
 bot.on('error', e => { console.error(e); });
-
-bot.login("MjYzODI1MTY1Mzg3ODI1MTUz.C0YAsA.u_eQETAWuHQfMu2ot957ZepDAx8");
