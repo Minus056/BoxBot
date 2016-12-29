e6ef7f1d4e235c576584923a1314515a906c00ca
Minus <kaushik.katta@gmail.com>
2016-12-29 08:01:17 +0000

    added command file

--- bot.js
+++ bot.js
@@ -6,29 +6,30 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 var fs = require("fs");
 bot.login(pass);
-var console_chan_id = "263901361127686159";
 
+var console_chan_id = "263901361127686159";
 var console_chan = bot.channels.get(console_chan_id);
 
-
 var lineCounts = JSON.parse(fs.readFileSync('./lines.json', 'utf8'));
+var commands = JSON.parse(fs.readFileSync('./commands.json', 'utf8'));
 
-var act_tok = "(";
-var bot_activation_token_start = "(";
-var bot_activation_token_stop = ")";
+var act_tok = "&";
+var bot_activation_token_start = "((";
+var bot_activation_token_stop = "))";
 
-
+/*=========================================================================*/
 function double_console(text)
 {
     console.log(text);
     bot.channels.get(console_chan_id).sendMessage("``` " + text + " ```");
 }
-
+/*=========================================================================*/
 
 bot.on('ready', () => 
 {
   double_console('I am ready!');
 });
+/*=========================================================================*/
 
 bot.on("message", msg => {
     if(msg.author.bot) return;
@@ -44,16 +45,14 @@ bot.on("message", msg => {
     lineCounts[msg.author.id]["wpl"] = userData.wpl;
     fs.writeFile('./lines.json', JSON.stringify(lineCounts), console.error);
 });
-
-
+/*=========================================================================*/
 
 bot.on("message", msg => {
-     if (msg.content.startsWith("ping")) {
-         msg.channel.sendMessage("pong!");
+     if (commands[msg.content]) {
+         msg.channel.sendMessage(commands[msg.content]);
      }
-    
 });
-
+/*=========================================================================*/
 
 bot.on('error', e => 
 { 
