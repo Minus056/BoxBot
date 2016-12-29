6f739add32cb9a0c5583ce69d44d7048d7f59ad2
Minus <kaushik.katta@gmail.com>
2016-12-28 20:00:17 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,21 +2,17 @@ require("env.js");
 
 var user = process.ENV.username; //no idea if i did this right
 //===============================
-var Discord = require('discord.io');
-var bot = new Discord.Client({
-    token: "MjYzODI1MTY1Mzg3ODI1MTUz.C0Xqqw.g_eqVUMfPm7Vz4BeTx2ikBxHMp8", //shouldn't that be hidden?
-    autorun: true
-});
- 
-bot.on('ready', function() { 
-    console.log(bot.username + " - (" + bot.id + ")");
-});
- 
-bot.on('message', function(user, userID, channelID, message, event) {
-    if (message === "ping") {
-        bot.sendMessage({
-            to: channelID,
-            message: "pong"
-        });
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+bot.on("message", msg => {
+    if (msg.content.startsWith("ping")) {
+        msg.channel.sendMessage("pong!");
     }
 });
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+bot.login("MjYzODI1MTY1Mzg3ODI1MTUz.C0Xqqw.g_eqVUMfPm7Vz4BeTx2ikBxHMp8");
