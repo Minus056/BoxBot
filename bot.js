7b588c3efc9b4c33859c88a1ef92531a48dc3865
sdgu <sdgu000@gmail.com>
2016-12-28 21:01:46 -0800

    env pass

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-//require("env.js");
+require("./env.js");
 
-// var pass = process.ENV.pass; //no idea if i did this right
+var pass = process.ENV.pass; //no idea if i did this right
 //===============================
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var fs = require("fs");
-bot.login("MjYzODI1MTY1Mzg3ODI1MTUz.C0YAsA.u_eQETAWuHQfMu2ot957ZepDAx8");
+bot.login(pass);
 var lineCounts = JSON.parse(fs.readFileSync('lines.json', 'utf8'));
 
 bot.on('ready', () => {
