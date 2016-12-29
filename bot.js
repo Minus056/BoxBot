23fdb8e402b5b1b2c607bd3eebfee1564e2012cb
Minus <kaushik.katta@gmail.com>
2016-12-28 20:01:37 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,14 +5,14 @@ var user = process.ENV.username; //no idea if i did this right
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
 bot.on("message", msg => {
     if (msg.content.startsWith("ping")) {
         msg.channel.sendMessage("pong!");
     }
-});
-
-bot.on('ready', () => {
-  console.log('I am ready!');
 });
 
 bot.login("MjYzODI1MTY1Mzg3ODI1MTUz.C0Xqqw.g_eqVUMfPm7Vz4BeTx2ikBxHMp8");
