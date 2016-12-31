d63bc0b0ed77f10a5d862747454dcda85a1a0ac1
Minus <kaushik.katta@gmail.com>
2016-12-31 22:19:24 +0000

    organized

--- bot.js
+++ bot.js
@@ -25,77 +25,7 @@ var itemList = require("./data/items.js");
 var lineFile = './Lines/'+month+'/'+day+'.json';
 var lineCounts = JSON.parse(fs.readFileSync(lineFile, 'utf8'));
 
-<<<<<<< HEAD
 var act_tok = "!";
-=======
-
-
-var itemList = require("./data/items.js");
-
-
-// var usage_data = require("./data/gen7ou-1825.json");
-// double_console(usage_data.data.Weavile.Items);
-
-// var test_url = "http://www.smogon.com/stats/2016-11/moveset/gen7ou-1825.txt";
-
-// var request = require("request");
-// request(
-// {
-//     url: test_url,
-//     json: false
-// }, function(e, res, body)
-// {
-//     console.log(body.indexOf("| Weavile                                | "));
-// })
-
-var act_tok = "(";
->>>>>>> 693c4473e3246c6d0f9fa121cd714fa7f43631a8
-
-var base_url = "http://www.smogon.com/stats/2016-11/moveset/";
-bot.on("message", function(msg)
-{
-    if (msg.content.startsWith(act_tok + "stats"))
-    {
-        var args = msg.content.split(" ");
-        // check for argc
-        var tier = args[1];
-        var rating = args[2];
-        var mon = args[3];
-
-        var search_mon = " | " + mon;
-        // 40 chars
-        var diff = 40 - mon.length - 1;
-        for (i = 0; i < diff; i++)
-        {
-            search_mon += " ";
-        }
-        search_mon += "| ";
-
-        var url = base_url + "gen7" + tier + "-" + rating + ".txt";
-        var request = require("request");
-
-        request(
-        {
-            url: url,
-            json: false
-        }, function(e, res, body)
-        {
-            if (!e && res.statusCode === 200)
-            {
-                var startInd = body.indexOf(search_mon);
-                var endInd = body.indexOf(" | Checks and Counters                    | ", startInd);
-                var sendStr = body.substring(startInd, endInd);
-                msg.channel.sendMessage(sendStr);
-                msg.channel.sendMessage("whu");
-            }
-            
-            // double_console(startInd + " " + endInd);
-            // double_console(body.substring(startInd, endInd));
-        });
-
-    }
-});
-
 
 /*=========================================================================*/
 //BOT READY
@@ -114,28 +44,6 @@ var checkApproved = function(msg) {
     }
     return false;
 };
-/*=========================================================================*/
-//COMMANDS WITH ARGS
-bot.on("message", function(msg)
-{
-    if (msg.content.startsWith(act_tok + "fx"))
-    {
-        var args = msg.content.split(" ");
-        var person1 = args[1];
-        var person2;
-        if (args[2] == undefined)
-        {
-            person2 = "me";
-        }
-        else
-        {
-            person2 = args[2];
-        }
-        msg.channel.sendMessage("hey " + person1 + " can you stop being a fucking asshole every time you talk to "  + 
-            person2 + " . fuck you cant talk to " + person2 + " without belittling " + person2  + 
-            " for no reason i dont give 2 shits if youre joking. you need to learn your limit asshole");
-    }
-});
 /*=========================================================================*/
 //LINE AND WPL COUNTER
 bot.on("message", function(msg)
@@ -243,16 +151,6 @@ bot.on("message", function(msg)
             leaderboardText  += bot.users.get(array[i][0]).username + "  |  " + array[i][1].lineCount + "  |  " + (Math.round((array[i][1].wpl * 100)) / 100) + "\n";
         }
         msg.channel.sendMessage(leaderboardText);
-    }
-});
-/*=========================================================================*/
-//COMMAND RESPONSES
-bot.on("message", function(msg)
-{
-    if (msg.author.bot) return;
-    if (commands[msg.content])
-    {
-        msg.channel.sendMessage(commands[msg.content]);
     }
 });
 /*=========================================================================*/
@@ -373,76 +271,6 @@ bot.on("message", msg => {
     }
 });
 /*=========================================================================*/
-<<<<<<< HEAD
-=======
-//LINE AND WPL COUNTER
-// bot.on("message", function(msg)
-// {
-//     if (msg.author.bot) return;
-
-//     var serverData = lineCounts[msg.guild.id];
-//     if (serverData == undefined)
-//     {
-//         lineCounts[msg.guild.id] = {};
-//     }
-//     var userData = lineCounts[msg.guild.id][msg.author.id];
-//     if (userData == undefined)
-//     {
-//         lineCounts[msg.guild.id][msg.author.id] = {
-//             "lineCount": 0,
-//             "wpl": 1
-//         };
-//         userData = {
-//             "lineCount": 0,
-//             "wpl": 1
-//         };
-//     }
-//     userData.wpl = ((userData.wpl * userData.lineCount) + (msg.content.split(" ").length)) / (userData.lineCount + 1);
-//     userData.lineCount++;
-//     lineCounts[msg.guild.id][msg.author.id]["lineCount"] = userData.lineCount;
-//     lineCounts[msg.guild.id][msg.author.id]["wpl"] = userData.wpl;
-//     fs.writeFile(lineFile, JSON.stringify(lineCounts), console.error);
-// });
-/*=========================================================================*/
-//LEADERBOARD
-
-bot.on("message", function(msg)
-{
-    if (msg.content == (act_tok+"leaderboard") && (msg.member.roles.has(adminRoleID) || msg.member.roles.has(SSRoleID) || msg.member.roles.has(mmmModRole)))
-    {
-        var array = [];
-        var members = Object.keys(lineCounts[msg.guild.id]);
-        for (var i = 0; i < members.length; i++)
-        {
-            array.push([members[i], lineCounts[msg.guild.id][members[i]]]);
-        }
-
-        array.sort(function(a,b){
-            return (b[1].lineCount*b[1].wpl) - (a[1].lineCount*a[1].wpl);
-
-        });
-        var leaderboardText = "```name | linecount | words/line\n";
-        leaderboardText += "ordered by number of words\n```"
-        var max = 10;
-        if (array.length < 10)
-        {
-            max = array.length;
-        }
-        for (var i = 0; i < max; i++)
-        {
-            leaderboardText  += bot.users.get(array[i][0]).username + "  |  " + array[i][1].lineCount + "  |  " + (Math.round((array[i][1].wpl * 100)) / 100) + "\n";
-        }
-        msg.channel.sendMessage(leaderboardText);
-    }
-
-    if (msg.content == act_tok+"resetlb" && (msg.member.roles.has(adminRoleID) || msg.member.roles.has(mmmModRole))) {
-
-        lineCounts[msg.guild.id] = {};
-        fs.writeFile(lineFile, JSON.stringify(lineCounts), console.error);
-        msg.channel.sendMessage("leaderboard reset");
-    }
-});
-/*=========================================================================*/
 //COMMAND RESPONSES
 bot.on("message", function(msg)
 {
@@ -453,9 +281,92 @@ bot.on("message", function(msg)
     }
 });
 /*=========================================================================*/
-
->>>>>>> 693c4473e3246c6d0f9fa121cd714fa7f43631a8
+//COMMANDS WITH ARGS
+bot.on("message", function(msg)
+{
+    if (msg.content.startsWith(act_tok + "fx"))
+    {
+        var args = msg.content.split(" ");
+        var person1 = args[1];
+        var person2;
+        if (args[2] == undefined)
+        {
+            person2 = "me";
+        }
+        else
+        {
+            person2 = args[2];
+        }
+        msg.channel.sendMessage("hey " + person1 + " can you stop being a fucking asshole every time you talk to "  + 
+            person2 + " . fuck you cant talk to " + person2 + " without belittling " + person2  + 
+            " for no reason i dont give 2 shits if youre joking. you need to learn your limit asshole");
+    }
+});
+/*=========================================================================*/
 bot.on('error', function(e)
 {
     console.error(e);
 });
+/*=========================================================================*/
+/*
+// var usage_data = require("./data/gen7ou-1825.json");
+// double_console(usage_data.data.Weavile.Items);
+
+// var test_url = "http://www.smogon.com/stats/2016-11/moveset/gen7ou-1825.txt";
+
+// var request = require("request");
+// request(
+// {
+//     url: test_url,
+//     json: false
+// }, function(e, res, body)
+// {
+//     console.log(body.indexOf("| Weavile                                | "));
+// })
+
+
+
+var base_url = "http://www.smogon.com/stats/2016-11/moveset/";
+bot.on("message", function(msg)
+{
+    if (msg.content.startsWith(act_tok + "stats"))
+    {
+        var args = msg.content.split(" ");
+        // check for argc
+        var tier = args[1];
+        var rating = args[2];
+        var mon = args[3];
+
+        var search_mon = " | " + mon;
+        // 40 chars
+        var diff = 40 - mon.length - 1;
+        for (var i = 0; i < diff; i++)
+        {
+            search_mon += " ";
+        }
+        search_mon += "| ";
+
+        var url = base_url + "gen7" + tier + "-" + rating + ".txt";
+        var request = require("request");
+
+        request(
+        {
+            url: url,
+            json: false
+        }, function(e, res, body)
+        {
+            if (!e && res.statusCode === 200)
+            {
+                var startInd = body.indexOf(search_mon);
+                var endInd = body.indexOf(" | Checks and Counters                    | ", startInd);
+                var sendStr = body.substring(startInd, endInd);
+                msg.channel.sendMessage(sendStr);
+                msg.channel.sendMessage("whu");
+            }
+            
+            // double_console(startInd + " " + endInd);
+            // double_console(body.substring(startInd, endInd));
+        });
+
+    }
+});*/
